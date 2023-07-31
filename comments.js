8d13eb523dd59e1a47d2ea60f1b0003b7476ca37
IamHendy <puchhendy@gmail.com>
2023-07-31 10:36:55 +0300

    variables and data types

--- comments.js
+++
@@ -1,9 +0,0 @@
-//My name is Hendrica
-
-/*
-I am abundant
-Iam confident
-I take my power
- */
-
-console.log("I am understanding javacript concepts");
---
+++ variables and data types/index.js
@@ -0,0 +1,9 @@
+//variables store data
+//We can implement variables in two  steps; declaration and assignmment.
+//there are three types  variables namely string, numbes and boolean.
+//variables are declared with the words "var", "let" and "const"
+
+let firstname = "Hendrica";
+let lastname = "Ogema";
+let age = 23;
+let nationality = "Kenyan";
